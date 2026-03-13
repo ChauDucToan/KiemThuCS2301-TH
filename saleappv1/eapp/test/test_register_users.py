4874291c184bd0eb0307ce6e5008ac1b75455524
Oslamelon <2351050182toan@ou.edu.vn>
2026-03-13 16:09:56 +0700

    test avatar

--- saleappv1/eapp/test/test_register_users.py
+++ saleappv1/eapp/test/test_register_users.py
@@ -5,6 +5,12 @@ from eapp.dao import add_user
 from eapp.models import User
 import hashlib
 
+@pytest.fixture
+def mock_cloudinary(monkeypatch):
+    def fake_image(file):
+        return {'secure_url': "https://hello.png"}
+    monkeypatch.setattr('cloudinary.uploader.upload', fake_image)
+
 def test_add_user(test_session):
     name = "abcd"
     username = "tester"
@@ -26,3 +32,14 @@ def test_existed_user(test_session):
     add_user(name=name, username=username, password=password, avatar=None)
     with pytest.raises(ValueError):
         add_user(name=name, username=username, password=password, avatar=None)
+
+def test_avatar(test_session, mock_cloudinary):
+    name = "abcd"
+    username = "tester"
+    password = "Admin@123"
+
+    add_user(name=name, username=username, password=password, avatar="aaaaa")
+
+    u = User.query.filter(username.__eq__(username)).first()
+    assert u
+    assert u.avatar == "https://hello.png"
